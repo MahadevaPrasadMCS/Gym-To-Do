8768c2a15a97b48c3c33509890f3e7a15da90c1b
MahadevaPrasadMCS <mahadevaprasadcs23@gmail.com>
2025-11-25 01:19:53 +0530

    Frontend request url added

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,7 +32,7 @@ app.use("/api/daily-logs", dailyLogsRoutes);
 app.use("/api/foods", foodRoutes);
 app.use("/api/weekly-checkins", weeklyCheckinsRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 // Connect DB + Seed Default Foods
 mongoose
