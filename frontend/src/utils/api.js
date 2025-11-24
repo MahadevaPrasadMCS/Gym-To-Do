04cba685d7c55ef119f904612a723455447ad722
MahadevaPrasadMCS <mahadevaprasadcs23@gmail.com>
2025-11-25 01:27:29 +0530

    Added server url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "http://localhost:5000/api" || "https://gym-to-do.onrender.com/api",
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
